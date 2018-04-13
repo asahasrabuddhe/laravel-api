73c1106d17e56b9321efe50c366a7f9272d1a65e
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2018-04-13 16:24:35 +0530

    Added Custom Base Middleware

---
+++ src/Middleware/BaseMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Asahasrabuddhe\LaravelAPI\Middleware;
+
+use Closure;
+use Asahasrabuddhe\LaravelAPI\BaseResponse;
+use Asahasrabuddhe\LaravelAPI\Exceptions\UnauthorizedException;
+use Illuminate\Support\Str;
+use Illuminate\Http\Response;
+
+class BaseMiddleware
+{
+
+    public function handle($request, Closure $next)
+    {
+        // Add CORS headers
+        $response = $next($request);
+
+        if ($response->getStatusCode() == 403 && ($response->getContent() == "Forbidden" || Str::contains($response->getContent(), ['HttpException', 'authorized']))) {
+            $response = BaseResponse::exception(new UnauthorizedException());
+        }
+
+        if (config("api.allowCors") && !$response instanceof StreamedResponse) {
+            $response->header('Access-Control-Allow-Origin', implode(',', config('api.allowedCorsOrigins')))
+                ->header('Access-Control-Allow-Methods', implode(',', config('api.allowedCorsMethods')))
+                ->header('Access-Control-Allow-Headers', implode(',', config('api.allowedCorsHeaders')));
+        }
+
+
+        return $response;
+    }
+}
